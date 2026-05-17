e3c7f6e18a453821f9202e8455da2ea6e30282b6
spencer kim <spencerkim1235@gmail.com>
2026-05-16 20:25:24 -0400

    feat: add mapLayer and showTraffic state to mapStore

--- src/store/mapStore.ts
+++ src/store/mapStore.ts
@@ -1,29 +1,35 @@
 import { create } from 'zustand'
 
+export type MapLayer = 'street' | 'dark' | 'satellite'
+
 interface MapState {
-  center: [number, number] // [latitude, longitude]
+  center: [number, number]
   zoom: number
   selectedStoreId: string | null
   isMapLoading: boolean
+  mapLayer: MapLayer
+  showTraffic: boolean
 
-  // Actions
   setCenter: (center: [number, number]) => void
   setZoom: (zoom: number) => void
   setSelectedStoreId: (storeId: string | null) => void
   setIsMapLoading: (loading: boolean) => void
+  setMapLayer: (layer: MapLayer) => void
+  setShowTraffic: (show: boolean) => void
 }
 
 export const useMapStore = create<MapState>((set) => ({
-  center: [39.8283, -98.5795], // Center of USA
+  center: [39.8283, -98.5795],
   zoom: 4,
   selectedStoreId: null,
   isMapLoading: false,
+  mapLayer: 'street',
+  showTraffic: false,
 
   setCenter: (center) => set({ center }),
-
   setZoom: (zoom) => set({ zoom }),
-
   setSelectedStoreId: (storeId) => set({ selectedStoreId: storeId }),
-
   setIsMapLoading: (loading) => set({ isMapLoading: loading }),
+  setMapLayer: (mapLayer) => set({ mapLayer }),
+  setShowTraffic: (showTraffic) => set({ showTraffic }),
 }))
